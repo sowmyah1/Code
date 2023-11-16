e06abeb63759f1833071a5c5f8f2bc385287d6fb
Sowmya <sowmyah@testvagrant.com>
2023-11-16 14:37:24 +0530

    substring

---
+++ code1/src/main/java/org/example/SubString.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class hemanth {
+    public static boolean betterString(String qwerty,String ans){
+        return qwerty.matches("(.*)"+ans+"(.*)");
+    }
+    public static boolean stringmatch2(String qwerty,String ans){
+        return qwerty.contains(ans);
+    }
+
+    public static void main(String[] args) {
+        System.out.println( betterString("hemathantt","ant"));
+        System.out.println(stringmatch2("qwerty","  "));
+    }
+}
