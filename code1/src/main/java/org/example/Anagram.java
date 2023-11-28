5d3690fec23f1cbed009961bf8c3e8d717143d63
Sowmya <sowmyah@testvagrant.com>
2023-11-28 16:11:18 +0530

    numberofcharnotequal

--- code1/src/main/java/org/example/Anagram.java
+++ code1/src/main/java/org/example/Anagram.java
@@ -1,2 +1,64 @@
-package org.example;public class Anagram {
+package org.example;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Anagram {
+    public static boolean isAnagram(String a,String b)
+    {
+
+        // Your code here
+        Set<Character> map = new LinkedHashSet<>();
+        Set<Character> set = new LinkedHashSet<>();
+        if(a.length() ==0 || b.length()==0 || a.length() != b.length()){
+            return false;
+        }
+        char[] a1 = a.toCharArray();
+        char[] b1 = b.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(b1);
+        for(int i=0;i<a1.length;i++){
+            System.out.print(a1[i]);
+        }
+        System.out.println();
+        for(int i=0;i<a1.length;i++){
+            System.out.print(b1[i]);
+        }
+
+        return !b1.equals(a1);
+//        for(int i=0;i<a.length();i++){
+//            set.add(a.charAt(i));
+//            map.add(b.charAt(i));
+//        }
+//        if(map.size() != set.size()){
+//            return false;
+//        }
+//
+//        HashMap<Character,Integer> map1 = new HashMap<>();
+//        HashMap<Object, Object> map2 = new HashMap<>();
+//        for(int i=0;i<a.length();i++){
+//            map1.put(a.charAt(i),1);
+//            System.out.println("the key is  of first string"+map1.get(a.charAt(i)) +" "+ a.charAt(i));
+//            map2.put(b.charAt(i),1);
+//            System.out.println("the key is second string "+map2.get(a.charAt(i))+" "+b.charAt(i));
+//        }
+//        return map1.equals(map2);
+
+//        System.out.println("the eqality"+map1.equals(map2));
+//        return set.equals(map);
+//        for(int i=0;i<b.length();i++){
+//            if(!map.contains(b.charAt(i))){
+//                return false;
+//            }
+//        }
+//        return true;
+    }
+
+    public static void main(String[] args) {
+       boolean b= isAnagram("geekforgeeks","forgeekgeeks");
+        System.out.println(b);
+    }
 }
--- code1/src/main/java/org/example/dutchflag.java
+++ code1/src/main/java/org/example/dutchflag.java
@@ -1,5 +1,32 @@
 package org.example;
 
 public class dutchflag {
-
+    public static void dutchNationalFlag(int[] a){
+        int i=0,j=0,k=a.length-1;
+        int temp=0;
+        System.out.println("qwed");
+        for(i=0;i<a.length;i++){
+            j=i;
+        while(j<k && j<a.length){
+            System.out.println(a[j]+"im for  loop");
+            if(a[j]>a[k]){
+                temp=a[k];
+                a[k]=a[j];
+                a[j]=temp;
+                j++;
+                k--;
+                System.out.println(a[j]);
+            }
+            else {
+                k--;
+            }
+        }}
+        for(int m=0;m<a.length;m++){
+            System.out.println(a[m]+ " im m");
+        }
+    }
+    public static void main(String[] args) {
+        int[] a = {0,2,1,0,1};
+         dutchNationalFlag(a);
+    }
 }
