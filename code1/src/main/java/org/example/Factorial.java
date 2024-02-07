cb9b61ebfd4fa610d6be8c14d9122cb011a78eb0
Sowmya <sowmyah@testvagrant.com>
2024-02-07 12:55:29 +0530

    working on Plus one to array problem

--- code1/src/main/java/org/example/Factorial.java
+++ code1/src/main/java/org/example/Factorial.java
@@ -2,4 +2,19 @@ package org.example;
 
 public class Factorial {
 
+    public static int climbStairs(int n) {
+        int a;
+        if(n<2){
+            return 1;
+        }
+        else{
+            System.out.println(n+" im before");
+             a= (climbStairs(n-1)+climbStairs(n-2));
+            System.out.println(n +" im after");
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        System.out.println(climbStairs(6));
+    }
 }
